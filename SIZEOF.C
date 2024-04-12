046e75ce452ab4d383354bccbfeb056af3da8195
aadithya2007 <166837708+aadithya2007@users.noreply.github.com>
2024-04-12 20:12:29 +0530

    Add files via upload

---
+++ SIZEOF.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+printf("size of integer:%ld byte\n",sizeof(int));
+printf("size of real number:%ld byte\n",sizeof(float));
+printf("size of charecters:%ld byte\n",sizeof(char));
+printf("size of double:%ld byte",sizeof(double));
+getch();
+}
